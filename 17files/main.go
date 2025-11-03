2b54f14fdf202636cdab245cb9a0f3e7517bc0ee
Vaibhav <vaibhav@Vaibhavs-MacBook-Air.local>
2025-11-03 22:56:24 +0530

    Learnt how to make web requests in golang

--- 17files/main.go
+++ 17files/main.go
@@ -3,6 +3,7 @@ package main
 import (
 	"fmt"
 	"io"
+	"io/ioutil"
 	"os"
 )
 
@@ -17,10 +18,30 @@ func main() {
 	}
 
 	length, err := io.WriteString(file, content)
-	if err != nil {
-		panic(err)
-	}
+	// if err != nil {
+	// 	panic(err)
+	// }
+	checkNilErr(err)
 
 	fmt.Println("Length is : ", length)
 	defer file.Close()
+
+	readFile("./sample.txt")
+}
+
+func readFile(filename string) {
+	// ioutil is deprecated. Look for what is used inplace of it now.
+	databyte, err := ioutil.ReadFile(filename)
+	// if(err != nil) {
+	// 	panic(err)
+	// }
+
+	checkNilErr(err)
+	fmt.Println("Text data inside the file is \n", string(databyte))
+}
+
+func checkNilErr(err error) {
+	if err != nil {
+		panic(err)
+	}
 }
